0b8dcf1357078379730763f8e493ada0939461a2
zanjs <root@zanjs.com>
2017-02-20 15:05:56 +0800

    :cat:

---
+++ server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+)
+
+func init() {
+	SysInit() //做系统初始化
+}
+
+func main() {
+	m := martini.Classic()
+
+	//m.RunOnAddr("ip:por")		//设置ip地址
+	//测试中间件
+	m.Use(testMid)
+	//中间件
+	m.Use(render.Renderer(render.Options{
+		Layout: "layout",
+	}))
+
+	//404的页面
+	m.NotFound(func() string {
+		return "傻逼,没找到东西吧?哈哈哈哈"
+	})
+
+	m.Get("/render", func(r render.Render) {
+		r.HTML(200, "hello", "大哥")
+	})
+
+	m.Get("/", func() string {
+		return "Hello world!"
+	})
+
+	m.Get("/index", func(r render.Render) {
+		r.HTML(200, "hello", "大哥")
+	})
+
+	m.Run()
+}
+
+func testMid(c martini.Context, l *log.Logger) {
+	l.Println("这是测试中间件,哈哈哈哈")
+	c.Next()
+	l.Println("after a request")
+}
---
+++ syService.go
@@ -0,0 +1,11 @@
+package main
+
+import "log"
+
+func SysInit() {
+	initLog() //初始化日志
+}
+
+func initLog() {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
+}
